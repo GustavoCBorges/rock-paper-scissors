f0410817916c41d8dc17d3aeb09a8c924ff1b710
Gustavo <gustavo.cordeiro.borges26@gmail.com>
2022-03-10 15:41:33 -0300

    Created the playRound function, wich receives the player input and  automatically chooses rock, paper or scissor as the computer choice, playing a single round of the game

--- javascript.js
+++ javascript.js
@@ -1,18 +1,62 @@
-function computerPlay() {
-    const rock = "Rock"
-    const paper = "Paper"
-    const scissors = "Scissors"
+const rock = "Rock"
+const paper = "Paper"
+const scissors = "Scissors"
 
+let computerSelection
+
+function computerPlay() {
     let randomNumber = Math.floor(Math.random()*3)
     if (randomNumber == 0) {
-        console.log(rock)
+        computerSelection = rock
     } else if (randomNumber == 1) {
-        console.log(paper)
+        computerSelection = paper
     } else {
-        console.log(scissors)
+        computerSelection = scissors
     }
 }
 
-function playRound() {
+console.log(computerPlay())
 
+function playRound() {
+    const playerSelection = prompt("Choose rock, paper or scissors!").toLowerCase()
+    computerPlay()
+    console.log(`You choose ${playerSelection} and...`)
+    if (playerSelection == "rock") {
+        switch (computerSelection) {
+            case rock:
+                console.log("it's a draw!")
+                break
+            case paper:
+                console.log("you lose! Paper beats rock")
+                break
+            case scissors:
+                console.log("you win! Rock beats paper")
+                break
+        }
+    } else if (playerSelection == "paper") {
+        switch (computerSelection) {
+            case rock:
+                console.log("you win! Paper beats rock")
+                break
+            case paper:
+                console.log("it's a draw!")
+                break
+            case scissors:
+                console.log("you lose! Scissors beats paper")
+                break
+        }
+    } else if (playerSelection == "scissors") {
+        switch (computerSelection) {
+            case rock:
+                console.log("you lose! Rock beats scissors")
+                break
+            case paper:
+                console.log("you win! Scissors beats paper")
+                break
+            case scissors:
+                console.log("it's a draw!")
+        }
+    } else {
+        console.log("well...you have to chose rock, paper or scissors...")
+    }
 }
