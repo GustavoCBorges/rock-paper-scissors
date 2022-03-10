573e4176eb9cd684b3c38a8dccef0b27540cabb0
Gustavo <gustavo.cordeiro.borges26@gmail.com>
2022-03-10 13:15:13 -0300

    Changed the computerPlay function to work with the variables rock, paper, scissors instead of the numbers

--- javascript.js
+++ javascript.js
@@ -1,10 +1,18 @@
 function computerPlay() {
+    const rock = "Rock"
+    const paper = "Paper"
+    const scissors = "Scissors"
+
     let randomNumber = Math.floor(Math.random()*3)
     if (randomNumber == 0) {
-        console.log("Rock!")
+        console.log(rock)
     } else if (randomNumber == 1) {
-        console.log("Paper!")
+        console.log(paper)
     } else {
-        console.log("Scissor!")
+        console.log(scissors)
     }
+}
+
+function playRound() {
+
 }
