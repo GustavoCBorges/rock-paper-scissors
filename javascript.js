a1ec045b9030cf5558b6a2530899972777074288
Gustavo <gustavo.cordeiro.borges26@gmail.com>
2022-03-11 11:20:26 -0300

    Changed the computerPlay and playRound functions to work with an  array

--- javascript.js
+++ javascript.js
@@ -1,62 +1,49 @@
-const rock = "Rock"
-const paper = "Paper"
-const scissors = "Scissors"
-
-let computerSelection
-
 function computerPlay() {
-    let randomNumber = Math.floor(Math.random()*3)
-    if (randomNumber == 0) {
-        computerSelection = rock
-    } else if (randomNumber == 1) {
-        computerSelection = paper
-    } else {
-        computerSelection = scissors
-    }
+    const computerOptions = ["rock","paper","scissors"]
+    const random = Math.floor(Math.random() * computerOptions.length)
+    return computerOptions[random]
 }
-
-console.log(computerPlay())
 
 function playRound() {
     const playerSelection = prompt("Choose rock, paper or scissors!").toLowerCase()
-    computerPlay()
+    const computerSelection = computerPlay()
     console.log(`You choose ${playerSelection} and...`)
     if (playerSelection == "rock") {
         switch (computerSelection) {
-            case rock:
-                console.log("it's a draw!")
+            case "rock":
+                return ("it's a draw!")
                 break
-            case paper:
-                console.log("you lose! Paper beats rock")
+            case "paper":
+                return ("you lose! Paper beats rock")
                 break
-            case scissors:
-                console.log("you win! Rock beats paper")
+            case "scissors":
+                return ("you win! Rock beats paper")
                 break
         }
     } else if (playerSelection == "paper") {
         switch (computerSelection) {
-            case rock:
-                console.log("you win! Paper beats rock")
+            case "rock":
+                return ("you win! Paper beats rock")
                 break
-            case paper:
-                console.log("it's a draw!")
+            case "paper":
+                return ("it's a draw!")
                 break
-            case scissors:
-                console.log("you lose! Scissors beats paper")
+            case "scissors":
+                return ("you lose! Scissors beats paper")
                 break
         }
     } else if (playerSelection == "scissors") {
         switch (computerSelection) {
-            case rock:
-                console.log("you lose! Rock beats scissors")
+            case "rock":
+                return ("you lose! Rock beats scissors")
                 break
-            case paper:
-                console.log("you win! Scissors beats paper")
+            case "paper":
+                return ("you win! Scissors beats paper")
                 break
-            case scissors:
-                console.log("it's a draw!")
+            case "scissors":
+                return ("it's a draw!")
         }
     } else {
-        console.log("well...you have to chose rock, paper or scissors...")
+        return ("well...you have to chose rock, paper or scissors...")
     }
 }
